3b246f0da4fdebeb0e1bb2d91dd388024dad8ca1
rhit-yangh6 <yangh6@rose-hulman.edu>
2020-12-11 00:28:25 +0800

    Optionals

--- miniExamples/02_Optionals.playground/contents.swift
+++ miniExamples/02_Optionals.playground/contents.swift
@@ -6,39 +6,55 @@ let dave = "Dave"
 
 // Part 1.
 // Optionals
+var optionalFloat: Float?
+var requiredFloat: Float
+var implicitlyUnwrappedOptionalFloat: Float!
 
-
-
+optionalFloat = 5.0
+requiredFloat = 5.0
+print("optionalFloat = \(optionalFloat)")
+print("requiredFloat = \(requiredFloat)")
 
 
 // Optionals with forced unwrapping
 
+print("optionalFloat = \(optionalFloat!)")
+let name = "Dave"
+let number = "7"
 
+var optionalThatIsNil: Int? = Int(name)
+var requiredThatIsNotNil: Int = Int(number)!
 
-
-
-
-
-
-
+optionalThatIsNil
+requiredThatIsNotNil
 
 // Part 2.
 // Views in a Playground + Optional Chaining
 
+import UIKit
 
+let b = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+b.backgroundColor = UIColor.red
 
+b.setTitle("Press Me", for: UIControl.State.normal)
 
+print(b.titleLabel)
+print(b.titleLabel?.text) // Optional Chaining
+print(b.titleLabel!.text) // Force Unwrapping
 
-
-
+//print(b.titleLabel!.text!)
 
 // Optional Binding
 
-
-
-
-
-
-
+if let myText = b.titleLabel?.text {
+    print("There is text and the value is \(myText)")
+} else {
+    print("There is no text on the label")
+}
 
 // Implicitly Unwrapped Optionals
+
+let myImplicitlyUnwrappedOptionalLabel: UILabel! = b.titleLabel
+
+print(myImplicitlyUnwrappedOptionalLabel.text)
+print(myImplicitlyUnwrappedOptionalLabel.text!)
