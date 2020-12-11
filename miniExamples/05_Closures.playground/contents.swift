9a86122d4c57c6c55a216a492c6f30476519450e
rhit-yangh6 <yangh6@rose-hulman.edu>
2020-12-11 18:48:12 +0800

    Closures

--- miniExamples/05_Closures.playground/contents.swift
+++ miniExamples/05_Closures.playground/contents.swift
@@ -2,29 +2,36 @@ var values = [10, 5, 2, 8, 3, 6, 1, 1000]
 
 // Verbose Closure
 
+//func myReducer(runningTotal: Int, currentValue: Int) -> Int {
+//    if currentValue > 5 {
+//        return runningTotal + 1
+//    } else {
+//        return runningTotal
+//    }
+//}
 
+//var numValuesOver5 = values.reduce(0, myReducer)
 
 
 
 // Closure parameter name shorthand and trailing closure
 
+//var numValuesOver9 = values.reduce(0) {(runningTotal, currentValue) in
+//    currentValue > 9 ? runningTotal + 1 : runningTotal
+//}
 
-
-
-
+var numValuesOver9 = values.reduce(0) { $1 > 9 ? $0 + 1 : $0 }
 
 // A Closure using a closure for a variable
 
+let numValuesOverLimit = {(myArray: [Int], limit: Int) -> Int in
+    return myArray.reduce(0) {(runningTotal, currentValue) in
+        return currentValue > limit ? runningTotal + 1 : runningTotal
+    }
+}
 
-
-
-
-
-
-
-
-//numValuesOverLimit(values, limit: 5)
-//numValuesOverLimit(values, limit: 9)
+numValuesOverLimit(values, 5)
+numValuesOverLimit(values, 9)
 
 
 // Comparing func and closure syntax
