e77ed74ad74274ab1e4a377ab9479d5e6b542c93
rhit-yangh6 <yangh6@rose-hulman.edu>
2020-12-11 20:59:07 +0800

    Advanced Classes

--- miniExamples/07_Classes.playground/contents.swift
+++ miniExamples/07_Classes.playground/contents.swift
@@ -45,14 +45,37 @@ BankAccount.getNumberOfBankAccounts()
 
 // Subclass
 
+class AtmBankAccount: BankAccount {
+    var withdrawFee: Double
+    
+    override convenience init(name: String, balance: Double) {
+        self.init(name: name, balance: balance, withdrawFee: 2.0)
+    }
+    
+    init(name: String, balance: Double, withdrawFee: Double) {
+        self.withdrawFee = withdrawFee
+        super.init(name: name, balance: balance)
+    }
+    
+    convenience init() {
+        self.init(name: "Unknown", balance: 0, withdrawFee: 2.0)
+    }
+    
+    override func withdraw(amount: Double) {
+        super.withdraw(amount: amount)
+        self.balance -= self.withdrawFee
+    }
+}
 
+//var bobAccount = AtmBankAccount(name: "Bob", balance: 100, withdrawFee: 33)
 
+var bobAccount = AtmBankAccount()
+bobAccount.name = "Bob"
 
+bobAccount.deposit(amount: 100)
+bobAccount.withdraw(amount: 40)
 
-
-//var bobAccount = AtmBankAccount()
-//bobAccount.deposit(100)
-//bobAccount.withdraw(40)
+bobAccount
 
 
 
