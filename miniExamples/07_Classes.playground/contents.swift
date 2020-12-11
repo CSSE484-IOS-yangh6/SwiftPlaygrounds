f4d6b527b951aa908e729105b5c723567243e04d
rhit-yangh6 <yangh6@rose-hulman.edu>
2020-12-11 20:34:42 +0800

    Basic classes

--- miniExamples/07_Classes.playground/contents.swift
+++ miniExamples/07_Classes.playground/contents.swift
@@ -1,20 +1,47 @@
 // Simple class
 
+class BankAccount: CustomStringConvertible {
+    var name: String
+    var balance: Double
+    
+    static var numberOfBankAccounts = 0
+    
+    init(name: String, balance: Double) {
+        self.name = name
+        self.balance = balance
+        BankAccount.numberOfBankAccounts += 1
+    }
+    
+    static func getNumberOfBankAccounts() -> Int {
+        return BankAccount.numberOfBankAccounts
+    }
+    
+    func deposit(amount: Double) {
+        self.balance += amount
+    }
+    
+    func withdraw(amount: Double) {
+        self.balance -= amount
+    }
+    
+    var description: String {
+        return "\(self.name): $\(self.balance)"
+    }
+}
 
+var daveAccount = BankAccount(name: "Dave", balance: 100.00)
+daveAccount.name
+daveAccount.balance += 100
+daveAccount.balance
 
-
-
-
-
-
-//var daveAccount = BankAccount(name: "Dave", balance: 100.00)
 //daveAccount.deposit(50)
 //daveAccount.withdraw(10)
 
+daveAccount.deposit(amount: 50)
+daveAccount
 
-
-
-
+var daveAccount2 = BankAccount(name: "Dave2", balance: 200.00)
+BankAccount.getNumberOfBankAccounts()
 
 // Subclass
 
